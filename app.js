59b0ec0b3485964a184b75347539b9712cca4991
malekshehadeh <malek.shehadeh96@gmail.com>
2024-06-03 22:36:36 +0300

    firstversion

---
+++ app.js
@@ -0,0 +1,66 @@
+console.log(-5 * 3);
+console.log("javascript" + 50);
+console.log(17 % 5);
+console.log(5 % 17);
+console.log(5 / 10);
+console.log(4 === "4");
+console.log(4 != 5);
+console.log(7 <= 8);
+console.log("Hello" + 5);
+let x = 10.5;
+console.log(Math.ceil(x) - Math.floor(x));
+console.log(Math.pow(x, 2));
+
+// let Q = prompt("Enter Number : ");
+// alert("Number " + Q);
+
+// let number1 = prompt("Enter Number 1  : ");
+// let number2 = prompt("Enter Number 2 : ");
+// let maximum = Math.max(number1 , number2);
+// let minumem = Math.min(number1 , number2);
+//   alert(minumem);
+//   alert(maximum);
+
+// let num1 = prompt("Enter Number one : ");
+// let num2 = prompt("Enter Number two : ");
+// let max = Math.max(num1, num2);
+// alert("the largest numbers" + max);
+
+// let number1=parseInt(prompt('Enter Number One : '));
+// let number=parseInt(prompt('Enter Number two : '));
+// let sum=number1+number;
+// alert('the sumation is ' + sum );
+
+// let number =7 ;
+// switch (number) {
+//   case 1:
+//     alert(1);
+//     break;
+//   case 2:
+//     alert(2);
+//     break;
+//   case 3:
+//     alert(3);
+//     break;
+//   case 4:
+//     alert(4);
+//     break;
+//   case 5:
+//     alert(5);
+//     break;
+//   case 6:
+//     alert(6);
+//     break;
+//   case 7:
+//     alert(7);
+//     break;
+//   case 8:
+//     alert(8);
+//     break;
+//   case 9:
+//     alert(9);
+//     break;
+//   default:
+//     alert("PLEASE TRY AGAIN");
+//     break;
+// }
